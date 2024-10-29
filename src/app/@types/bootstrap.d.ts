5cf5e6f16e0378129c5834bb7af3e0342e486208
Sandro Santos <sandro.santos@radialdc.local>
2024-10-29 00:14:01 -0300

    hadsjkadhsdjkashdk

--- src/app/@types/bootstrap.d.ts
+++ src/app/@types/bootstrap.d.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'bootstrap/dist/js/bootstrap.bundle.min.js' {
-    const bootstrap: any;
+    const bootstrap: any; // eslint-disable-line @typescript-eslint/no-explicit-any
     export default bootstrap;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
