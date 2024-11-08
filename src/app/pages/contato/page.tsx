d71a745e1682b22a4ddb49743d1a873768669adf
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2024-11-07 23:45:18 -0300

    kldaskdlasdsakldjl

--- src/app/pages/contato/page.tsx
+++ src/app/pages/contato/page.tsx
@@ -100,12 +100,10 @@ export default function Contato() {
                 width="600"
                 height="450"
                 style={{ border: "0" }}
-                allowFullScreen=""
+                allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
-
-
             </div>
           </div>
         </div>
